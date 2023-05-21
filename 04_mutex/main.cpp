ac53d6d84d4a790e3fb9c837c4d313ae1e9d8e9a
Rachit Garg <rachitgarg900@gmail.com>
2023-05-21 19:49:08 +0200

    Improved 04 with better example

--- 04_mutex/main.cpp
+++ 04_mutex/main.cpp
@@ -7,12 +7,23 @@
 #include <mutex>
 #include <chrono>
 
-static int x {}; // Shared variable initialized to 0 (no need of static, but still just to ensure)
+int x {}; // Shared variable initialized to 0
 std::mutex m; // Mutex needed to access critical region
 
-// try commenting m.lock() and m.unlock() statements
-// It may or may not increment to 2 in that case
-void increment()
+// try commenting m.lock() and m.unlock() statements in both functions
+// It will give result as 1 instead of 2 in that case
+
+void increment_slow()
+{
+    m.lock();                                             // Start of Critical Region
+    int temp {x};                                         // Critical Region
+    ++temp;                                               // Critical Region
+    std::this_thread::sleep_for(std::chrono::seconds(2)); // Critical Region
+    x = temp;                                             // Critical Region
+    m.unlock();                                           // End of Critical Region
+}
+
+void increment_fast()
 {
     m.lock();   // Start of Critical Region
     ++x;        // Critical Region
@@ -24,8 +35,8 @@ int main()
 
     std::cout << "Value of x before t1 and t2 started : " << x << std::endl; // should be 0
 
-    std::thread t1(increment);
-    std::thread t2(increment);
+    std::thread t1(increment_slow);
+    std::thread t2(increment_fast);
 
     t1.join();
     t2.join();
